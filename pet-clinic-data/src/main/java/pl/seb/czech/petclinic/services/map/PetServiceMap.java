002d7ff69237d67ae3436f87ef0a8b4ec78b75d4
Sebastian C <seb.czech@wp.pl>
2020-02-25 14:30:27 +0100

    Add Map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/pl/seb/czech/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package pl.seb.czech.petclinic.services.map;
+
+import pl.seb.czech.petclinic.model.Pet;
+import pl.seb.czech.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long>  implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
